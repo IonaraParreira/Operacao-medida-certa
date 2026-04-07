c82e14bb5488f47fb0c2d2e95f8ecf8f388ce644
NullSpider <235911627+IonaraParreira@users.noreply.github.com>
2026-04-07 13:53:30 -0300

    Úlima chance

--- medida-certa.py
+++ medida-certa.py
@@ -1,3 +1,5 @@
+from colorama import Fore,Style,init
+init(autoreset=True)
 peso = float(input("Digite seu peso (kg): "))
 altura_input = input("Digite sua altura (m): ")
 altura = float(altura_input.replace(',','.'))
@@ -6,8 +8,8 @@ imc = peso / (altura ** 2)
 print(f"Seu IMC é: {imc:.2f}")
 
 if imc < 18.5:
-    print("Alerta: O sistema detectou vulnerabilidade por baixo peso.")
+    print(f"{Fore.YELLOW}⚠️ Alerta:{Style.RESET_ALL}O sistema detectou vulnerabilidade por baixo peso.")
 elif imc < 25:
-    print("Acesso Permitido: Você está protegido e saudável,junte-se aos sobreviventes e faça parte da repopulação da humanidade.")
+    print(f"{Fore.GREEN}Acesso Permitido:{Style.RESET_ALL} Você está protegido e saudável,junte-se aos sobreviventes e faça parte da repopulação da humanidade.")
 else:
-    print("Acesso Negado: O sistema detectou humano infectado,recomenda medidas de segurança imediatas.Praticar mais atividades físicas,reduzir açúcar e ingerir bastante água.")
+    print(f"{Fore.RED}Acesso Negado:{Style.RESET_ALL} O sistema detectou {Fore.RED}humano infectado☣️{Style.RESET_ALL}, recomenda medidas de segurança imediatas.{Fore.BLUE}Praticar mais atividades físicas,reduzir açúcar e ingerir bastante água.{Style.RESET_ALL}")
